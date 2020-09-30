cb0cfc2273a3c139c746cd039d1082576771a670
Nowo Adi Suryo <37267507+nowoadisuryo@users.noreply.github.com>
2020-09-30 16:38:20 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,42 @@
+const config = {
+  title: "Voting Application"
+}
+
+const submissions = [
+  {
+    id: 1,
+    title: "Yellow",
+    description: "Lorem ipsum dolor sit amet.",
+    url: "javascript:void(0)",
+    votes: 0,
+    avatar: "public/images/avatars/daniel.jpg",
+    submissionImage: "public/images/submissions/image-yellow.png",
+  },
+  {
+    id: 2,
+    title: "Steel",
+    description: "Lorem ipsum dolor sit amet.",
+    url: "javascript:void(0)",
+    votes: 0,
+    avatar: "public/images/avatars/kristy.png",
+    submissionImage: "public/images/submissions/image-steel.png",
+  },
+  {
+    id: 3,
+    title: "Rose",
+    description: "Lorem ipsum dolor sit amet.",
+    url: "javascript:void(0)",
+    votes: 0,
+    avatar: "public/images/avatars/molly.png",
+    submissionImage: "public/images/submissions/image-rose.png",
+  },
+  {
+    id: 4,
+    title: "Aqua",
+    description: "Lorem ipsum dolor sit amet.",
+    url: "javascript:void(0)",
+    votes: 0,
+    avatar: "public/images/avatars/veronika.jpg",
+    submissionImage: "public/images/submissions/image-aqua.png",
+  },
+]
